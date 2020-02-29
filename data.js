b4cc392d137c72f4d9e146398d191f93f761f43e
arifedenizli <61147794+arifedenizli@users.noreply.github.com>
2020-02-29 17:29:00 +0100

    Update data.js

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/c0776d16-6873-4c32-7fb7-f869a5f72b0d-b646/",
                 "target": "_blank"
             }
         ]
